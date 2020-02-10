1a2bd30a641054239445b347de75a33b6d9ef0ab
Skye Geraghty <skyetay03@googlemail.com>
2020-02-10 07:19:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,7 +20,8 @@ def reduce(source, output = 0)
     
     index += 1
   end
-  
+  # I feel really badly about the following line of Code
+  # I intend to revisit this when my knowledge is better and correct it
   if output == nil
     output = false
   end
