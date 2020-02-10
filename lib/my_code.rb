e3f75bf0be21ff16b7d8dc21b34565bd3d3997e3
Skye Geraghty <skyetay03@googlemail.com>
2020-02-10 06:37:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,22 @@
 # Your Code Here
+
+def map(source)
+  output = []
+  index = 0
+  
+  while index < source.length do
+    output.push(yield(source[index]))
+    index += 1
+  end
+  return output
+end
+
+def reduce(source, output = 0)
+  index = 0
+  
+  while index < source.length do
+    yield(output, source[index])
+    index += 1
+  end
+  output
+end
